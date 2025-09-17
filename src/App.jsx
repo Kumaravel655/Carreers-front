3adc63802463da2db21724a2461d7a61849246c9
Murugan <mathimuruganam@gmail.com>
2025-09-17 19:36:57 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+
 import Home from "./pages/Home";
 import "./styles/global.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecruiterLoginPage from './Pages/RecruiterLoginPage/RecruiterLoginPage';
+import ForgotPasswordPage from './Pages/ForgotPasswordPage/ForgotPasswordPage';
+import ResetPasswordPage from './Pages/ResetPasswordPage/ResetPasswordPage';
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element =  {<Home/>}/>
+        <Route path="/login" element={<RecruiterLoginPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
