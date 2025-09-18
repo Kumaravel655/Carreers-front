5c8f252448e019b648817b704783aad717c932af
Rahul-BE <royal0rahul@gmail.com>
2025-09-18 18:13:59 +0530

    Faq component updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,25 @@ import AppliedJobsPage from "./pages/AppliedJobsPage";
 import JobAlertsPage from "./pages/JobAlertsPage";
 import FavoriteJobsPage from "./pages/FavoriteJobsPage";
 import SettingsPage from "./pages/SettingsPage";
+import Faq from "./Components/Faq/Faq";
 function App ()
 {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/employee-prof" element = {<Dashboard/>}/>
-        <Route path = "/Recruiter-login" element= {<RecruiterLoginPage/>}/>
-         <Route path="/dashboard" element ={<DashboardPage/>}/>
-         <Route path="/applied-jobs" element ={<AppliedJobsPage/>}/>
-         <Route path="/job-alerts" element ={<JobAlertsPage/>}/>
-         <Route path="/favorite-jobs" element ={<FavoriteJobsPage/>}/>
-         <Route path="/settings" element ={<SettingsPage/>}/>
-        <Route path="/findjob" element={<FindJob />} />
-        <Route path="/jobs/:jobId" element={<JobDesc />} />
-      </Routes>
-    </Router>
+    <Faq/>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home/>}/>
+    //     <Route path="/employee-prof" element = {<Dashboard/>}/>
+    //     <Route path = "/Recruiter-login" element= {<RecruiterLoginPage/>}/>
+    //      <Route path="/dashboard" element ={<DashboardPage/>}/>
+    //      <Route path="/applied-jobs" element ={<AppliedJobsPage/>}/>
+    //      <Route path="/job-alerts" element ={<JobAlertsPage/>}/>
+    //      <Route path="/favorite-jobs" element ={<FavoriteJobsPage/>}/>
+    //      <Route path="/settings" element ={<SettingsPage/>}/>
+    //     <Route path="/findjob" element={<FindJob />} />
+    //     <Route path="/jobs/:jobId" element={<JobDesc />} />
+    //   </Routes>
+    // </Router>
   );
 }
 
